b8276957d21e0852a3d548560480b5320a51ed0c
Your Name <you@example.com>
2024-03-27 22:42:58 -0400

    mongoDB setup

--- app.py
+++ app.py
@@ -2,3 +2,16 @@
 
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+# MongoDB setup
+app.config["MONGO_URI"] = "mongodb://localhost:27017/todoDatabase"
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
